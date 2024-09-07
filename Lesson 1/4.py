45167da777968b43c10aa584d82139556f5cba61
kostyakopchan <108663362+kostyakopchan@users.noreply.github.com>
2024-09-07 22:40:00 +0300

    Add files via upload

---
+++ Lesson 1/4.py
@@ -0,0 +1,8 @@
+#first way
+name = ("Konstantsin")
+print("Hello, my name is " + name)
+
+#second way
+name = ("Konstantsin")
+message = ("Hello, my name is ")
+print(message + name)
---
+++ Lesson 1/5.py
@@ -0,0 +1,10 @@
+#main solution
+name = input("Please enter your name:")
+surname = input("Please enter your surname:")
+full_name = name.title() + " " + surname.title()
+print("Hello, my name is " + full_name)
+
+#alternative solution
+name = input("Please enter your name:")
+surname = input("Please enter your surname:")
+print("Hello, my name is " + name.title() + " " + surname.title())
