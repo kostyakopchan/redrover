07bf9ccc100f60554b793d756b99cb04b1111d8a
kostyakopchan <kopchan.k@gmail.com>
2024-10-06 21:29:51 +0300

    Some adjustments to lesson 6 hometask and adds calculator

---
+++ Lesson 6/Calculator.py
@@ -0,0 +1,36 @@
+class Calculator:
+    operations_history = []
+    def __init__(self):
+        operations_history = []
+    def add(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+        result = self.num1 + self.num2
+        self.operations_history.append(f"{self.num1} + {self.num2} = {result}")
+        return result
+    
+    def mul(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+        result = self.num1 * self.num2
+        self.operations_history.append(f"{self.num1} * {self.num2} = {result}")
+        return result
+    
+    def div(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+        result = self.num1 / self.num2
+        self.operations_history.append(f"{self.num1} / {self.num2} = {result}")
+        return result
+    
+    def show_operations(self):
+        print(self.operations_history)
+
+    def clear(self):
+        self.operations_history = []
+        
+calc = Calculator()
+calc.add(1, 3)
+calc.add(2, 10)
+calc.mul(100, 12.2)
+calc.show_operations()
--- Lesson 6/test_bookings.py
+++ Lesson 6/test_bookings.py
@@ -4,15 +4,12 @@ BOOKING_URL = 'https://restful-booker.herokuapp.com/booking'
 
 def test_get_bookings():
     response = requests.get(BOOKING_URL)
-    if response.status_code == 200:
-        print("Get Bookings - PASSED")
-    else:
-        print("Get Bookings - FAILED")
+    assert response.status_code == 200, "Bookings not received"
 
 def test_booking_creation():
     created_booking = new_booking()
     booking_info = requests.get(BOOKING_URL + '/' + str((created_booking['bookingid'])))
-    print(booking_info.json())
+    assert booking_info.json()['firstname'] == created_booking['booking']['firstname'], "Booking not created"
 
 def test_booking_patch():
     created_booking = new_booking()
@@ -21,11 +18,13 @@ def test_booking_patch():
                                    headers={"Cookie" : "token=" + token},
                                    json={"firstname" : "Konstantinpatched",
                                         "lastname": "Kopchanpatched"})
-    print(booking_patch.json())
+    assert booking_patch.json()['firstname'] == 'Konstantinpatched', "Name not patched"
+    assert booking_patch.json()['lastname'] == 'Kopchanpatched', 'Lastname not patched'
 
 def test_booking_deletion():
     created_booking = new_booking()
     token = get_token()
     booking_deletion = requests.delete(BOOKING_URL + '/' + str((created_booking['bookingid'])), 
                                    headers={"Cookie" : "token=" + token})
-    print(requests.get(BOOKING_URL + '/' + str((created_booking['bookingid']))))
+    response = requests.get(BOOKING_URL + '/' + str((created_booking['bookingid'])))
+    assert response.status_code == 404, 'Booking not deleted'
